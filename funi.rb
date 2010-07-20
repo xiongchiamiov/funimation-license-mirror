decc64bcc1a0562fd565b41f9b32b6de4d192fc9
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-07-20 01:45:00 -0700

    I guess I'll have to go with (x)html over markdown, for simplicity's sake

--- funi.rb
+++ funi.rb
@@ -5,5 +5,5 @@ require 'nokogiri'
 doc = Nokogiri::HTML(open('http://funimation.com/f_index.cfm?page=copy'))
 
 doc.css('td p').slice(2..-2).each do |p|
-	puts p.content << '  '
+	puts p.content << '<br />'
 end

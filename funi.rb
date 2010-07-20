6453449376a07dd5222ae1b8bf81890b1b467fc5
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-07-20 00:44:25 -0700

    initial commit

---
+++ funi.rb
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open('http://funimation.com/f_index.cfm?page=copy'))
+
+doc.css('td p').slice(2..-2).each do |p|
+	puts p.content << '  '
+end

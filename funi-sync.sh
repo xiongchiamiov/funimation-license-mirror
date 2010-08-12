5452b11bc399b44f84cef3071acb31ac397f334c
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-08-12 03:35:18 -0700

    fix sync script

--- funi-sync.sh
+++ funi-sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ~/Documents/funimation-license-mirror
-git checkout master && git pull && cp funi.py /tmp/funi.py
-git checkout gh-pages && git pull && python /tmp/funi.py > index.html && git commit -am 'automated update' && git push
+git checkout master && git pull && python funi.py > /tmp/index.html
+git checkout gh-pages && git pull && cp /tmp/index.html index.html && git commit -am 'automated update' && git push

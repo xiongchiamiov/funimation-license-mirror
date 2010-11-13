c3a20abddb14d8698e788a5f5d4ed4030d4cf901
Xiong Chiamiov <xiong.chiamiov@gmail.com>
2010-11-12 16:37:54 -0800

    checkout master at end of sync script, else script won't be available to run next time

--- funi-sync.sh
+++ funi-sync.sh
@@ -3,3 +3,4 @@
 cd ~/Documents/funimation-license-mirror
 git checkout master && git pull && python funi.py > /tmp/index.html
 git checkout gh-pages && git pull && cp /tmp/index.html index.html && git commit -am 'automated update' && git push
+git checkout master

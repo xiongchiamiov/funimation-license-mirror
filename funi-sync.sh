2c51fc488a67a7fc800ec0c27b05a93d7db7399a
xiongchiamiov <xiong.chiamiov@gmail.com>
2010-11-17 01:47:18 -0800

    specify py2k

--- funi-sync.sh
+++ funi-sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ~/Documents/funimation-license-mirror
-git checkout master && git pull && python funi.py > /tmp/index.html
+git checkout master && git pull && python2 funi.py > /tmp/index.html
 git checkout gh-pages && git pull && cp /tmp/index.html index.html && git commit -am 'automated update' && git push

318390a7f6e1cfe749ddd6a3ef624bd9441fb491
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-05-29 15:34:16 -0700

    Add hashbang and benediction.

--- funi.py
+++ funi.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# May you recognize your weaknesses and share your strengths.
+# May you share freely, never taking more than you give.
+# May you find love and love everyone you find.
+
 from jinja2 import Environment, FileSystemLoader
 from pyquery import PyQuery
 
